685a247277e7507fb3bb8c97d3d5a7456b2b9a5f
M Widyan <m.widyan_34@yahoo.com>
2017-02-14 09:56:22 +0700

    Widyan first

---
+++ Tes.java
@@ -0,0 +1,14 @@
+class Tes{
+	
+	int panjang = 1;
+	int lebar = 1;
+
+	//TASK(1) buat method return value untuk menghasilkan keliling persegi panjang
+	//TASK(2) buat method return value untuk menghasilkan luas persegi panjang
+	//TASK(3) buat method untuk menampilkan keliling persegi panjang
+	//TASK(4) buat method untuk menampilkan luas persegi panjang
+	//TASK(5) buat main method
+
+
+
+}
